0c926f9f1975e82032e2370c23119b4377910779
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-19 09:47:44 +0600

    Experience done

---
+++ src/components/Experience.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import exp from "../images/exp.png";
+import imggg from '../images/lolo.png'
+
+const Experience = () => {
+  return (
+    <>
+      <div className="main container flex items-center justify-center gap-3 pt-[133px] pb-[440px]">
+        <div className="exp-img">
+          <img src={exp} alt="exp" />
+        </div>
+
+
+        <div className="exp-content">
+            <img src={imggg} alt="content" />
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default Experience;
