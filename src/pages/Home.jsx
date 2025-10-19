ed84e17e2ccc8ed23fa01ab97d880ad1301bfb24
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-19 10:13:07 +0600

    catagory done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Experience from '../components/Experience'
+import Catalog from '../components/Catalog'
 
 const Home = () => {
   return (
     <>
     <Banner/>
     <Experience/>
+    <Catalog/>
     
     </>
   )
