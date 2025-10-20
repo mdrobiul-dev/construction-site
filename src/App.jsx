c61ba6c7f2791ed12b7798d32d78b70f23f4038b
mdrobiul.dev <robiulhassanrobi33@gmail.com>
2025-10-20 08:05:48 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Teampage from "./pages/Teampage"
-
-
-import JOBS from './pages/JOBS'
-
-import Registration from "./pages/Registration"
-
-
-import Landingpage from './pages/Landingpage'
-import Portfolio from './pages/Portfolio'
-
-
+import Teampage from "./pages/Teampage";
+import JOBS from "./pages/JOBS";
+import Registration from "./pages/Registration";
+import Landingpage from "./pages/Landingpage";
+import Portfolio from "./pages/Portfolio";
 
 const App = () => {
   return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/jobs" element={<JOBS />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/team" element={<Teampage />} />
+      </Routes>
+    </Router>
+  );
+};
 
-<>
-      
-   
-
-  <Registration />
-
-     {/* <JOBS/> */}
-    
-    
-  <Portfolio />
-
-
- 
-
-   
-   {/* <Teampage /> */}
-   {/* <Landingpage /> */}
-    </>
-
-  )
-}
-
-export default App
+export default App;
