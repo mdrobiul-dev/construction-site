4823a0e97bbe56390e3d63e94fb7bf3a158e6941
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-20 10:13:30 +0600

    aos animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const App = () => {
+
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true, 
+    });
+  }, []);
+
+
+
   return (
     <>
     <Navbar/>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,12 +6,12 @@ const Experience = () => {
   return (
     <>
       <div className="main container flex items-center justify-center gap-3 pt-[133px] pb-[440px]">
-        <div className="exp-img">
+        <div className="exp-img" data-aos="fade-up">
           <img src={exp} alt="exp" />
         </div>
 
 
-        <div className="exp-content">
+        <div className="exp-content" data-aos="fade-down">
             <img src={imggg} alt="content" />
         </div>
 
